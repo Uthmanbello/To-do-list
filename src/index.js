246f92e45da776ff70b3aaa2f4c8072e2d34ccad
Uthmanbello <uthmanbello88@gmail.com>
2023-01-18 21:10:08 +0100

    Setup dynamic part of the page using JS

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import _ from 'lodash';
 import './style.css';
   
-// const element = document.createElement('div');
-// element.innerHTML = "Hi";
-// document.body.appendChild(element);
-
 const taskArr = [{
     description: "wash the dishes",
     completed: false,
     index: 0,
 },
 {
-    description: "complete To Do list project",
+    description: "take out the trash",
     completed: false,
     index: 1,
+},
+{
+    description: "complete To Do list project",
+    completed: false,
+    index: 2,
 }
-]
+];
+
+const container = document.querySelector('.task-container');
+
+let html = '';
+
+taskArr.forEach((task) => {
+  html += `
+        <div class="list-item">
+            <input type="checkbox" id="${task.index}" class="check">
+            <p class="task black">${task.description}</p>
+            <i class="fa-solid fa-ellipsis-vertical ash fa-2x"></i>
+        </div>`;
+});
+
+container.innerHTML = html;
