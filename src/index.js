a38a8341514ada947daf00488f2ac0c120886aed
Uthmanbello <uthmanbello88@gmail.com>
2023-01-18 17:54:39 +0100

    Make array of tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import _ from 'lodash';
 import './style.css';
   
-const element = document.createElement('div');
-element.innerHTML = "Hi";
-document.body.appendChild(element);
+// const element = document.createElement('div');
+// element.innerHTML = "Hi";
+// document.body.appendChild(element);
+
+const taskArr = [{
+    description: "wash the dishes",
+    completed: false,
+    index: 0,
+},
+{
+    description: "complete To Do list project",
+    completed: false,
+    index: 1,
+}
+]
