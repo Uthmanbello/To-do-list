4ca4ed313719b0375bd68e24be8ddcd52f082a14
Uthmanbello <uthmanbello88@gmail.com>
2023-01-20 00:48:29 +0100

    Setup add function

--- src/index.js
+++ src/index.js
@@ -1,34 +1,127 @@
 import _ from 'lodash';
 import './style.css';
 
-const taskArr = [{
-  description: 'wash the dishes',
-  completed: false,
-  index: 0,
-},
-{
-  description: 'take out the trash',
-  completed: false,
-  index: 1,
-},
-{
-  description: 'complete To Do list project',
-  completed: false,
-  index: 2,
-},
-];
+// const taskArr = [{
+//   description: 'wash the dishes',
+//   completed: false,
+//   index: 0,
+// },
+// {
+//   description: 'take out the trash',
+//   completed: false,
+//   index: 1,
+// },
+// {
+//   description: 'complete To Do list project',
+//   completed: false,
+//   index: 2,
+// },
+// ];
+// const refreshButton = document.querySelector('#refresh-btn');
+// const addButton = document.querySelector('#add-btn');
+// const dragButton = document.querySelector('#drag-btn');
+// const container = document.querySelector('.task-container');
+// const add = document.querySelector('.add');
+// refreshButton.addEventListener('click', myFunction)
+// function myFunction() {
+//     addButton.innerHTML = "YOU CLICKED ME!";
+//   }
+// const taskArr = [];
+// addTask = () => {
+//     task = {
+//         description: add.value,
+//         completed: false
+//     }
+//     taskArr.push(task)
+//     add.value = '';
+// }
 
-const container = document.querySelector('.task-container');
+// addButton.addEventListener('click', myFunction);
 
-let html = '';
+// const toDoTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+// task = {
+//     description: 
+// }
+// function add(task) {
+//     toDoTasks.push(task);
+//     localStorage.setItem('tasks', JSON.stringify(toDoTasks));
+// }
 
-taskArr.forEach((task) => {
-  html += `
-        <div class="list-item">
-            <input type="checkbox" id="${task.index}" class="check">
-            <p class="task black">${task.description}</p>
-            <i class="fa-solid fa-ellipsis-vertical ash fa-2x icon"></i>
-        </div>`;
-});
+// function remove(task) {
+//     let index = toDoTasks.indexOf(task);
+//     if (index !== -1) {
+//         toDoTasks.splice(index, 1);
+//         localStorage.setItem('tasks', JSON.stringify(toDoTasks));
+//     }
+// }
 
-container.innerHTML = html;
+// addButton.addEventListener("click", function() {
+//     let inputField = document.getElementById("input-field");
+//     add(inputField.value);
+// });
+
+
+// let html = '';
+
+// taskArr.forEach((task) => {
+//   html += `
+        // <div class="list-item">
+        //     <input type="checkbox" id="" class="check">
+        //     <p class="task black">${task.description}</p>
+        //     <i class="fa-solid fa-ellipsis-vertical ash fa-2x icon" id="drag-btn"></i>
+        // </div>`;
+// });
+
+// container.innerHTML = html;
+
+let tasks = {};
+const displayTasks = document.querySelector('.task-container');
+const newTask = document.querySelector('#input-field');
+const addTask = document.querySelector('#add-btn');
+
+let storeTasks = [];
+const clearField = () => {
+  newTask.value = '';
+};
+const displayAllTasks = () => {
+    clearField();
+    displayTasks.innerHTML = '';
+    storeTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    storeTasks.forEach((task, index) => {
+      displayTasks.innerHTML += `
+      <div class="list-item">
+      <input type="checkbox" class="check">
+      <p class="task black">${task.description}</p>
+      <button class="remove" id="${index}" onclick="removeTasks(event)"><i class="fa-solid fa-trash-can ash icon"></i><i class="fa-solid fa-ellipsis-vertical ash fa-2x icon d-none" id="drag-btn"></i></button>
+  </div>`;
+    });
+  };
+
+  const saveTasks = () => {
+    addTask.addEventListener('click', () => {
+      if (newTask.value === '') return;
+      tasks = {
+        description: newTask.value,
+        completed: false,
+        index: storeTasks.length,
+      };
+      storeTasks.push(tasks);
+      localStorage.setItem('tasks', JSON.stringify(storeTasks));
+      displayAllTasks();
+      clearField();
+    });
+  };
+
+  const removeTasks = (event) => {
+    const findId = Number(event.target.id);
+    const deleteTask = storeTasks.filter((task, index) => {
+      if (index !== findId) {
+        return task;
+      }
+    });
+    localStorage.setItem('tasks', JSON.stringify(deleteTask));
+    displayAllTasks();
+  };
+
+  displayAllTasks();
+  saveTasks();
