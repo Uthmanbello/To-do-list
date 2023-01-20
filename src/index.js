b821c836c040b3deffcf3b4335ae623a90dfdc61
Uthmanbello <uthmanbello88@gmail.com>
2023-01-20 19:24:27 +0100

    Add modules for adding, removing and editing items

--- src/index.js
+++ src/index.js
@@ -1,127 +1,71 @@
-import _ from 'lodash';
 import './style.css';
+import TodoList from './modules/todo.js';
 
-// const taskArr = [{
-//   description: 'wash the dishes',
-//   completed: false,
-//   index: 0,
-// },
-// {
-//   description: 'take out the trash',
-//   completed: false,
-//   index: 1,
-// },
-// {
-//   description: 'complete To Do list project',
-//   completed: false,
-//   index: 2,
-// },
-// ];
-// const refreshButton = document.querySelector('#refresh-btn');
-// const addButton = document.querySelector('#add-btn');
-// const dragButton = document.querySelector('#drag-btn');
-// const container = document.querySelector('.task-container');
-// const add = document.querySelector('.add');
-// refreshButton.addEventListener('click', myFunction)
-// function myFunction() {
-//     addButton.innerHTML = "YOU CLICKED ME!";
-//   }
-// const taskArr = [];
-// addTask = () => {
-//     task = {
-//         description: add.value,
-//         completed: false
-//     }
-//     taskArr.push(task)
-//     add.value = '';
-// }
+let todolist = [];
+if (JSON.parse(localStorage.getItem('todolist'))) {
+  todolist = JSON.parse(localStorage.getItem('todolist')).todolist;
+}
+const latestTodoList = new TodoList(todolist);
+const todos = document.getElementsByClassName('todolist')[0];
 
-// addButton.addEventListener('click', myFunction);
+const sortedTodoList = todolist.sort((a, b) => a.index - b.index);
 
-// const toDoTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-// task = {
-//     description: 
-// }
-// function add(task) {
-//     toDoTasks.push(task);
-//     localStorage.setItem('tasks', JSON.stringify(toDoTasks));
-// }
+sortedTodoList.forEach((todo) => {
+  const task = document.createElement('li');
+  task.classList.add('task');
+  task.id = todo.index;
+  task.innerHTML = `<input type="checkbox" name="${todo.index}" class="check"><label class = "${todo.index} task-desc black" for="${todo.index}">${todo.description}</label><div class="remove-button"><i class='fa fa-trash'></i><div>`;
+  todos.appendChild(task);
+});
 
-// function remove(task) {
-//     let index = toDoTasks.indexOf(task);
-//     if (index !== -1) {
-//         toDoTasks.splice(index, 1);
-//         localStorage.setItem('tasks', JSON.stringify(toDoTasks));
-//     }
-// }
+const enterButton = document.getElementById('submit-new-item');
 
-// addButton.addEventListener("click", function() {
-//     let inputField = document.getElementById("input-field");
-//     add(inputField.value);
-// });
-
-
-// let html = '';
-
-// taskArr.forEach((task) => {
-//   html += `
-        // <div class="list-item">
-        //     <input type="checkbox" id="" class="check">
-        //     <p class="task black">${task.description}</p>
-        //     <i class="fa-solid fa-ellipsis-vertical ash fa-2x icon" id="drag-btn"></i>
-        // </div>`;
-// });
-
-// container.innerHTML = html;
-
-let tasks = {};
-const displayTasks = document.querySelector('.task-container');
-const newTask = document.querySelector('#input-field');
-const addTask = document.querySelector('#add-btn');
-
-let storeTasks = [];
-const clearField = () => {
-  newTask.value = '';
+const addTask = (e) => {
+  e.preventDefault();
+  const inputForm = document.getElementById('new-item');
+  if (inputForm.value) {
+    const description = inputForm.value.trim();
+    const index = todolist.length + 1;
+    latestTodoList.addTask(description, index);
+    localStorage.setItem('todolist', JSON.stringify(latestTodoList));
+    inputForm.value = '';
+  }
+  window.location.reload();
 };
-const displayAllTasks = () => {
-    clearField();
-    displayTasks.innerHTML = '';
-    storeTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    storeTasks.forEach((task, index) => {
-      displayTasks.innerHTML += `
-      <div class="list-item">
-      <input type="checkbox" class="check">
-      <p class="task black">${task.description}</p>
-      <button class="remove" id="${index}" onclick="removeTasks(event)"><i class="fa-solid fa-trash-can ash icon"></i><i class="fa-solid fa-ellipsis-vertical ash fa-2x icon d-none" id="drag-btn"></i></button>
-  </div>`;
-    });
-  };
 
-  const saveTasks = () => {
-    addTask.addEventListener('click', () => {
-      if (newTask.value === '') return;
-      tasks = {
-        description: newTask.value,
-        completed: false,
-        index: storeTasks.length,
-      };
-      storeTasks.push(tasks);
-      localStorage.setItem('tasks', JSON.stringify(storeTasks));
-      displayAllTasks();
-      clearField();
-    });
-  };
+enterButton.addEventListener('click', addTask);
 
-  const removeTasks = (event) => {
-    const findId = Number(event.target.id);
-    const deleteTask = storeTasks.filter((task, index) => {
-      if (index !== findId) {
-        return task;
-      }
-    });
-    localStorage.setItem('tasks', JSON.stringify(deleteTask));
-    displayAllTasks();
-  };
+const editButton = document.querySelectorAll('.task');
+editButton.forEach((elm) => {
+  const element = elm.children[1];
+  element.addEventListener('click', () => {
+    element.contentEditable = true;
+    element.focus();
+  });
 
-  displayAllTasks();
-  saveTasks();
+  element.addEventListener('focusout', () => {
+    if (element.innerHTML) {
+      latestTodoList.editTask(element.innerHTML, element.className);
+      localStorage.setItem('todolist', JSON.stringify(latestTodoList));
+      element.contentEditable = false;
+    }
+  });
+  element.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter' && element.innerHTML) {
+      latestTodoList.editTask(element.innerHTML, element.className);
+      localStorage.setItem('todolist', JSON.stringify(latestTodoList));
+      element.contentEditable = false;
+    }
+  });
+});
+
+const removeButton = document.querySelectorAll('.remove-button');
+
+const removeTask = (e) => {
+  const index = e.target.parentNode.parentNode.id;
+  latestTodoList.removeTask(index);
+  localStorage.setItem('todolist', JSON.stringify(latestTodoList));
+  window.location.reload();
+};
+
+removeButton.forEach((element) => element.addEventListener('click', removeTask));
